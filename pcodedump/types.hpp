24b050f59854209198538bd4462e60d5f126734c
Craig McGeachie <slapdau@yahoo.com.au>
2023-06-26 21:16:39 +1200

    Align pCode file in memory on block size boundry.

--- pcodedump/types.hpp
+++ pcodedump/types.hpp
@@ -27,7 +27,49 @@ namespace pcodedump {
 
 	constexpr unsigned int BLOCK_SIZE = 512;
 
-	using buff_t = std::vector<std::uint8_t>;
+    template<typename T, std::align_val_t ALIGNMENT = static_cast<std::align_val_t>(sizeof(std::max_align_t))>
+    class AlignedAllocator
+    {
+        static_assert(static_cast<std::size_t>(ALIGNMENT) >= alignof(T), "Overalignment size too small for type T");
+
+    public:
+        using value_type = T;
+        static std::align_val_t constexpr ALIGNMENT{ ALIGNMENT };
+
+        using size_type = std::size_t;
+
+        template<class U>
+        struct rebind
+        {
+            using other = AlignedAllocator<U, ALIGNMENT>;
+        };
+
+        constexpr AlignedAllocator() noexcept = default;
+
+        constexpr AlignedAllocator(const AlignedAllocator&) noexcept = default;
+
+        template<typename U>
+        constexpr AlignedAllocator(AlignedAllocator<U, ALIGNMENT> const&) noexcept {}
+
+        static constexpr size_type max_size() {
+            return numeric_limits<std::size_t>::max() / sizeof(T);
+        }
+
+        [[nodiscard]] T* allocate(std::size_t n) {
+            if (n > max_size()) {
+                throw bad_array_new_length();
+            }
+            return reinterpret_cast<T*>(::operator new[](n * sizeof(T), ALIGNMENT));
+        }
+
+        void
+            deallocate(T* p, [[maybe_unused]] std::size_t n)
+        {
+            ::operator delete[](p, ALIGNMENT);
+        }
+    };
+
+    using buff_t = std::vector<std::uint8_t, AlignedAllocator<std::uint8_t, static_cast<std::align_val_t>(BLOCK_SIZE)>>;
 
 	inline std::uint8_t const * derefSelfPtr(void const * selfPtr) {
 		return static_cast<std::uint8_t const *>(selfPtr) - *reinterpret_cast<boost::endian::little_int16_t const *>(selfPtr);
